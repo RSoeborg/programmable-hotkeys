0379350db12b466c3a2a0d61f1c91ad80cf3681c
Rasmus <rsoeborg@gmail.com>
2018-10-02 02:20:21 +0200

    Disabled console logging.

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -4,10 +4,11 @@ import java.io.IOException;
 
 public class Program {
     public static void main(String[] args) throws IOException {
+        Provider.logger.setUseParentHandlers(false);
         HotkeyLoader loader = new HotkeyLoader("keymap.txt");
         HotkeyWindow hotkeyWindow = new HotkeyWindow(loader);
-
         Provider provider = Provider.getCurrentProvider(false);
+
         provider.register(KeyStroke.getKeyStroke("shift A"), hotKey -> {
             hotkeyWindow.show();
         });
